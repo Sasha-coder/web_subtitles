54135c8b84268baf5d2194666cc9362be9e2155b
Alexandra Tardova <aitardova@edu.hse.ru>
2023-12-11 22:55:21 +0300

    web done

--- web_sub/users/urls.py
+++ web_sub/users/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.home),
+    path('login/', views.open_login),
+    path('register/', views.open_register),
+    path('upload/', views.open_upload_page)
 ]
--- web_sub/users/views.py
+++ web_sub/users/views.py
@@ -2,8 +2,29 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
+def home(request):
+    return render(request, 'users/main_page.html')
+
+
+def open_login(request):
     return render(request, 'users/login.html')
 
 
-#<link>rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"</link>
+def open_register(request):
+    return render(request, 'users/registration.html')
+
+
+def open_upload_page(request):
+    return render(request, 'users/upload.html')
+
+
+def upload_video(request):
+    if request.method == 'POST':
+        uploaded_file = request.FILES['video_file']
+        # Вы можете сохранить файл или обработать его здесь
+        # Например:
+        # with open('some_path/video.mp4', 'wb+') as destination:
+        #     for chunk in uploaded_file.chunks():
+        #         destination.write(chunk)
+        return HttpResponse('Файл успешно загружен')
+    return render(request, 'upload.html')
